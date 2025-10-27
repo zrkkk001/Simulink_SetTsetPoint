81322cc0c7bbf7bdf087d1d566265388cbf49c36
zrkkk001 <81376923+zrkkk001@users.noreply.github.com>
2025-10-27 08:51:04 +0800

    Update SetTsetPoint.m

--- SetTsetPoint.m
+++ SetTsetPoint.m
@@ -2,8 +2,9 @@ chartName =get_param(gcbh,'Name');
     portHandles = get_param(gcbh,'portHandles');
 outportHandle = portHandles.Outport;
 for iii =1:length(outportHandle)
-set_param(outportHandle(iii),'TestPoint','on','Name','ass')
+set_param(outportHandle(iii),'TestPoint','on','Name','sss')
 ouname=get_param(outportHandle(iii),'PropagatedSignals')
 % set_param(outportHandle(iii),'Name',[ouname,'_',chartName(1:7)]);
   
 end
+
